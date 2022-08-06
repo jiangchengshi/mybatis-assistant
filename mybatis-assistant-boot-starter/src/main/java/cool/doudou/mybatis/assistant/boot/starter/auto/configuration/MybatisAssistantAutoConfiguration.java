efa9fb1d18bf4b01e4f78518b996c59708cb27a1
这么诚实 <jiangchengshi@gmail.com>
2022-08-07 00:14:39 +0800

    feat: 1.0.10 (spring-boot 2.6.10)

--- mybatis-assistant-boot-starter/src/main/java/cool/doudou/mybatis/assistant/boot/starter/auto/configuration/MybatisAssistantAutoConfiguration.java
+++ mybatis-assistant-boot-starter/src/main/java/cool/doudou/mybatis/assistant/boot/starter/auto/configuration/MybatisAssistantAutoConfiguration.java
@@ -3,11 +3,11 @@ package cool.doudou.mybatis.assistant.boot.starter.auto.configuration;
 import cool.doudou.mybatis.assistant.boot.starter.config.MybatisAssistantConfig;
 import cool.doudou.mybatis.assistant.boot.starter.helper.CodeHelper;
 import cool.doudou.mybatis.assistant.boot.starter.properties.MybatisAssistantProperties;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.Import;
  */
 @EnableConfigurationProperties(MybatisAssistantProperties.class)
 @Import({DataSourceAutoConfiguration.class, MybatisAssistantConfig.class})
-@AutoConfiguration
+@Configuration
 public class MybatisAssistantAutoConfiguration {
     @ConditionalOnMissingBean
     @Bean
